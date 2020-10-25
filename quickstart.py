6677a287afbcb453b5bc0bb82c7d3148fd49ccfa
Beau <bschelb@clemson.edu>
2020-10-24 21:00:05 -0400

    m

--- quickstart.py
+++ quickstart.py
@@ -36,25 +36,13 @@ agent = Agent.create(
     # Automatically configured network
     network='auto',
     # PPO optimization parameters
-    batch_size=10, update_frequency=2, learning_rate=3e-4, multi_step=10,
+    batch_size=20, update_frequency=5, learning_rate=3e-4, multi_step=10,
     subsampling_fraction=0.33,
-    # Reward estimation
-    likelihood_ratio_clipping=0.2, discount=0.99, predict_terminal_values=False,
-    # Baseline network and optimizer
-    baseline=dict(type='auto', size=32, depth=1),
-    baseline_optimizer=dict(optimizer='adam', learning_rate=1e-3, multi_step=10),
-    # Regularization
-    l2_regularization=0.0, entropy_regularization=0.0,
-    # Preprocessing
-    state_preprocessing='linear_normalization', reward_preprocessing=None,
     # Exploration
-    exploration=0.0, variable_noise=0.0,
+    exploration=0.1, variable_noise=0.0,
     # Default additional config values
     config=None,
     parallel_interactions=1,
-    summarizer=dict(directory='summaries', summaries='all'),
-    # Do not record agent-environment interaction trace
-    recorder=None
 )
 
 # Initialize the runner
